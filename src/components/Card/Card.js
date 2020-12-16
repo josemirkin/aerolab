0bd7c54c0337b65fd68664a29bb93cfea5bae2d5
josemirkin <jsoemirkin@gmail.com>
2020-12-16 16:57:34 -0300

    16-12 after work

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,18 +6,15 @@ import buyWhite from '../../images/buy-white.svg'
 
 import './Card.scss'
 
-function Card({category, cost, id, img, name}) {
+function Card({ category, cost, id, img, name }) {
     return (
         <div className="col">
             <div className="card">
-                <div className="buy-item-hover">
-                    <div className="d-flex justify-content-end">
-                        <img src={buyWhite} alt="buy" className="buy-logo"/>
-                    </div>
+                {/*<div className="buy-item-hover">
                     <div className="buy-menu">
                         <div className="buy-menu coins">
                             {cost}
-                                <img src={coinsLogo} alt="coins"/>
+                            <img src={coinsLogo} alt="coins" />
                         </div>
                         <div className="buy-menu button">Redeem now</div>
                     </div>
@@ -26,15 +23,20 @@ function Card({category, cost, id, img, name}) {
                     <div className="buy-menu">
                         <div className="buy-menu coins">
                             you need 1.700
-                                <img src={coinsLogo} alt="coins"/>
+                                <img src={coinsLogo} alt="coins" />
                         </div>
                         <div className="buy-menu button">Get coins</div>
                     </div>
                 </div>
                 <div className="d-flex justify-content-end">
-                    <img src={buyBlue} alt="buy" className="buy-logo buy-logo-disappear" />
+                        <img src={buyWhite} alt="buy" className="buy-logo" />
+                    </div>*/}
+                <div className="image-container">
+                    <div className="d-flex justify-content-end">
+                        <img src={buyBlue} alt="buy" className="buy-logo buy-logo-disappear" />
+                    </div>
+                    <img src={img} className="card-img-top" alt="..." />
                 </div>
-                <img src={img} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <p className="card-text">{category}</p>
                     <div className="card-title d-flex">
